4ce55165bbff7cb61e62fca49730e4e4fa92440f
TobyMike-max <osinubi.michael@gmail.com>
2023-01-11 02:44:54 +0100

    [Modified]: Changed keyword argument variable to one given in question

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,14 +6,14 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Function that returns a list of delays in ascending order
     Args:
         n(int): Number of times to spawn wait_random function
-        delay(int): Maximum time for wait random argument.
+        max_delay(int): Maximum time for wait random argument.
     Return:
         finish(List[float])
     """
-    tsk_obj = [task_wait_random(delay) for i in range(n)]
+    tsk_obj = [task_wait_random(max_delay) for i in range(n)]
     finish = [await task for task in asyncio.as_completed(tsk_obj)]
     return finish
