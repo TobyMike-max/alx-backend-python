3c7d3bf0416fe840cccb1ecf6ee8904b5bf72497
TobyMike-max <osinubi.michael@gmail.com>
2023-01-11 02:41:06 +0100

    [Modified]: Changed keyword argument

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,14 +6,14 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function that returns a list of delays in ascending order
     Args:
         n(int): Number of times to spawn wait_random function
-        delay(int): Maximum time for wait random argument.
+        max_delay(int): Maximum time for wait random argument.
     Return:
         finish(List[float])
     """
-    tsk_obj = [asyncio.create_task(wait_random(delay)) for i in range(n)]
+    tsk_obj = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     finish = [await task for task in asyncio.as_completed(tsk_obj)]
     return finish
