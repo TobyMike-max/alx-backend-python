da20a2f3a17d4bd2c24ef1cc3c270d8ab8131587
TobyMike-max <osinubi.michael@gmail.com>
2023-01-11 02:42:57 +0100

    [Modified]: Changed keyword argument variable to one given in question

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,7 +6,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Function the measures runtime of an async function
     Args:
         n(int): Number of times to call function
@@ -15,6 +15,6 @@ def measure_time(n: int, delay: int) -> float:
         total_time / n(float)
     """
     s = time.perf_counter()
-    asyncio.run(wait_n(n, delay))
+    asyncio.run(wait_n(n, max_delay))
     total_time = time.perf_counter() - s
     return total_time / n
