1c6d0f7e9ae0ed870d34e68af12d96a12557b433
TobyMike-max <osinubi.michael@gmail.com>
2023-01-11 05:51:26 +0100

    [Modified]: Changed incorrect module name

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,5 +8,5 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """Function that returns a list"""
     for i in range(10):
-        await async.sleep(1)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
