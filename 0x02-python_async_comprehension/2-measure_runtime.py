8c60f146607a8b5a8026dba4720c2105468cb81f
TobyMike-max <osinubi.michael@gmail.com>
2023-01-11 05:47:45 +0100

    [Create]: File for task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Run time for four parallel comprehensions"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Function that counts runtime"""
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    return time.perf_counter() - start
