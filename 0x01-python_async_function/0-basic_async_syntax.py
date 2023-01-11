dfb15eff7205b96d39afc7dd522781c870e40c68
TobyMike-max <osinubi.michael@gmail.com>
2023-01-11 02:38:02 +0100

    [Modified]: Changed keyword argument

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,13 +5,13 @@ import asyncio
 import random
 
 
-async def wait_random(delay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """An async function that sleeps within a random float
     Input:
         delay(int): Defaults to 10
     Return:
         delay(float)
     """
-    delay = random.uniform(0, delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
